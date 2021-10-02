c87ebca460f10038172bd7850327a2bb328278c5
Jay <jamied_uk@hotmail.com>
2021-10-02 16:02:08 +0100

    Create Buttons_Template.php

---
+++ Buttons_Template.php
@@ -0,0 +1,17 @@
+<style>
+.button {
+    cursor:pointer;
+    background-color: #4CAF50; /* Green */
+    border: none;
+    color: white;
+    padding: 15px 32px;
+    text-align: center;
+    text-decoration: none;
+    display: inline-block;
+    font-size: 16px;
+}
+</style>
+
+
+<a href="something" class="button">Button</a>
+<a href="something" class="button">It's getting old</a>	
